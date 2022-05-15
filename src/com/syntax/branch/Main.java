4965fe4963ec8bc4b504a81351b70fc173e96a74
eduardito2009 <cm655176@gmail.com>
2022-05-15 14:19:32 -0400

    Solar System Basic Ready

---
+++ src/com/syntax/branch/Main.java
@@ -0,0 +1,13 @@
+package com.syntax.branch;
+
+public class Main {
+    //write your code
+    public static void main(String[] args) {
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun="1";
+        FactorX.Moon="4";
+        FactorX.Planet="2";
+        FactorX.Stars="1000";
+
+    }
+}
